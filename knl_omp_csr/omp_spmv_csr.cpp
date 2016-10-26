7ea89887c4960d0875d953686d2a133fda4c83cc
scl <solvercorleone@gmail.com>
2016-10-27 00:18:00 +0900

    1st version of csr spmv without openmp

---
+++ knl_omp_csr/omp_spmv_csr.cpp
@@ -0,0 +1,26 @@
+#include "omp_spmv_csr.h"
+
+void spmv_csr(denseMat vec_result, csrMat mat, denseMat vec)
+{
+
+#ifdef SPMV_DEBUG_A
+	assert (vec_result.global_num_col == 1);
+	assert (vec.global_num_col == 1);
+#endif
+
+	IDX_TYPE mat_row_idx, mat_col_idx;
+
+	for (mat_row_idx = 0; mat_row_idx<mat.num_rows; mat_row_idx++)
+	{
+		IDX_TYPE i_start = mat.row_start[mat_row_idx];
+		IDX_TYPE i_end   = mat.row_start[mat_row_idx+1];
+		vec_result.data[mat_row_idx]=0.0;
+
+		for (mat_col_idx=i_start; mat_col_idx<i_end; mat_col_idx++)
+		{
+			vec_result.data[mat_row_idx] += mat.csrdata[mat_col_idx] 
+									 * vec.data[ mat.col_idx[mat_col_idx] ];
+		}
+	}
+
+}
--- knl_omp_csr/omp_spmv_csr.h
+++ knl_omp_csr/omp_spmv_csr.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "DataTypes.h"
+#include <assert.h>
+#include <omp.h>
 
 #ifndef VAL_TYPE
 #define VAL_TYPE double
@@ -10,10 +12,15 @@
 #define IDX_TYPE int
 #endif
 
+#ifndef SPMV_DEBUG
+#define SPMV_DEBUG_A
+
+#endif
+
 #ifdef	__cplusplus
 extern "C" {
 #endif
-
+	void spmv_csr(denseMat vec_result, csrMat mat, denseMat vec);
 
 #ifdef	__cplusplus
 }
